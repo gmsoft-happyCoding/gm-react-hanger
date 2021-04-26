5de242043651773cea3ca1009031de25913595e9
angular-moon <xiaoxuss@163.com>
2021-04-26 14:50:39 +0800

    improve useRestore
    fix history back issues

--- src/useRestore.ts
+++ src/useRestore.ts
@@ -1,69 +1,125 @@
-/* eslint-disable consistent-return */
-/* eslint-disable no-restricted-globals */
 /**
  * save the current application state to top.history.state
  * restore the status of the application when refreshing or rebounding
+ * Known problem: This solution causes the history stack to grow, but there is no better solution at the moment
  */
 import { useEffect } from 'react';
 import { History, Location } from 'history';
-import { debounce } from 'lodash';
 import HistoryHelper from 'history-helper';
+import { findIndex, isEqual } from 'lodash';
 
 const STATE_KEY = 'RESTORE_INNER_APP';
+
 let topHistoryHelper: HistoryHelper;
+
 try {
   topHistoryHelper = new HistoryHelper(STATE_KEY, top.history);
 } catch (e) {
   // nothing
 }
 
-interface State {
+type Action = 'push' | 'replace';
+interface RestoreDataI {
   location: Location<any>;
-  selfState: any;
+  selfStateStack: Array<{
+    url?: string;
+    state: any;
+  }>;
 }
 
-const saveSelfState = debounce(
-  (selfState: any) => {
-    if (self !== top && topHistoryHelper) topHistoryHelper.mergeState({ selfState });
-  },
-  1000,
-  {
-    leading: true,
-    trailing: true,
-  },
-);
+const getUrl = () => `${location.pathname}${location.search}${location.hash}`;
+
+/**
+ * why save the entire history stack?
+ * iframe pushed into the history stack
+ * the browser can not correctly back after refresh, will not trigger popstate event
+ * resulting in the route can not be correctly switched
+ */
+const saveSelfState = (action: Action, state: any, url?: string) => {
+  if (self !== top && topHistoryHelper) {
+    const selfStateStack = topHistoryHelper.getValue('selfStateStack', []);
+
+    if (action === 'push') {
+      selfStateStack.push({
+        url: url || getUrl(),
+        state,
+      });
+    } else {
+      selfStateStack[Math.max(selfStateStack.length - 1, 0)] = {
+        url: url || getUrl(),
+        state,
+      };
+    }
+
+    topHistoryHelper.shallowMergeState({ selfStateStack });
+  }
+};
 
 export const useRestore = (history: History<any>) => {
   /**
-   *  save location to top
+   * restore the status of the application
    */
   useEffect(() => {
-    // in top window
+    if (self === top || !topHistoryHelper) return;
+
+    const state = topHistoryHelper.getState();
+    if (state) {
+      const { location, selfStateStack } = state as RestoreDataI;
+
+      // restore router
+      if (location) history.replace(location);
+
+      /**
+       * restore history stack
+       * Known problem: This solution causes the history stack to grow, but there is no better solution at the moment
+       */
+      if (selfStateStack && selfStateStack.length > 0) {
+        const first = selfStateStack[0];
+        self.history.replaceState(first.state, '', first.url);
+
+        for (let i = 1; i < selfStateStack.length; i++) {
+          const item = selfStateStack[i];
+          self.history.pushState(item.state, '', item.url);
+        }
+      }
+    }
+  }, [history]);
+
+  /**
+   *  save location in top.history.state
+   */
+  useEffect(() => {
     if (self === top || !topHistoryHelper) return;
 
     // Listen for changes to the current location.
-    return history.listen(location => topHistoryHelper.mergeState({ location }));
+    return history.listen(location =>
+      topHistoryHelper.mergeState({
+        location,
+      }),
+    );
   }, [history]);
 
   /**
    * hacking by monkey patch
-   * save history.state to top
+   * save history.state in top.history.state
    */
   useEffect(() => {
-    // in top window
     if (self === top || !topHistoryHelper) return;
+
+    // save current route
+    saveSelfState('replace', self.history.state, getUrl());
 
     const originalReplaceState = self.history.replaceState;
 
     self.history.replaceState = (state: any, title: string, url?: string) => {
-      saveSelfState(state);
+      saveSelfState('replace', state, url);
       originalReplaceState.call(self.history, state, title, url);
     };
 
     const originalPushState = self.history.pushState;
 
     self.history.pushState = (state: any, title: string, url?: string) => {
-      saveSelfState(state);
+      saveSelfState('push', state, url);
       originalPushState.call(self.history, state, title, url);
     };
 
@@ -73,20 +129,27 @@ export const useRestore = (history: History<any>) => {
     };
   }, []);
 
-  /**
-   * restore the status of the application
-   */
+  // handle history back
   useEffect(() => {
-    // in top window
     if (self === top || !topHistoryHelper) return;
 
-    const state = topHistoryHelper.getState();
-    if (state) {
-      const { location, selfState } = state as State;
-      history.replace(location);
-      self.history.replaceState(selfState, '');
-    }
-  }, [history]);
+    window.addEventListener('popstate', event => {
+      const selfStateStack = topHistoryHelper.getValue('selfStateStack', []);
+      const url = getUrl();
+      const index = findIndex(selfStateStack, item =>
+        isEqual(item, {
+          state: event.state,
+          url: url,
+        }),
+      );
+
+      if (index > -1) {
+        topHistoryHelper.shallowMergeState({
+          selfStateStack: selfStateStack.slice(0, index + 1),
+        });
+      }
+    });
+  }, []);
 };
 
 export default useRestore;
